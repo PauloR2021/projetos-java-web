6eb93f40b132d20e454723272d6608e5d7805365
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-09-07 11:46:12 -0300

    adicionando a classe de model para criar uma cidade

---
+++ src/main/java/com/pr/software/crud_cidades/model/Cidade.java
@@ -0,0 +1,22 @@
+package com.pr.software.crud_cidades.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "cidades")
+@Getter @Setter
+public class Cidade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length=100)
+    private String nome;
+
+    @Column(nullable = false,length=100)
+    private String estado;
+
+}
---
+++ src/main/java/com/pr/software/crud_cidades/repository/CidadeRepository.java
@@ -0,0 +1,4 @@
+package com.pr.software.crud_cidades.repository;
+
+public interface CidadeRepository {
+}
