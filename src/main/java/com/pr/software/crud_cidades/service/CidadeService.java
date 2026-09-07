da461b0f5b37ac2ff0d38322dbec5b6db89fd374
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-09-07 13:47:54 -0300

    criando o service para o tratamento de regras

--- src/main/java/com/pr/software/crud_cidades/service/CidadeService.java
+++ src/main/java/com/pr/software/crud_cidades/service/CidadeService.java
@@ -1,4 +1,37 @@
 package com.pr.software.crud_cidades.service;
 
+import com.pr.software.crud_cidades.dtos.RequestCidade;
+import com.pr.software.crud_cidades.dtos.ResponseCidade;
+import com.pr.software.crud_cidades.model.Cidade;
+import com.pr.software.crud_cidades.repository.CidadeRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CidadeService {
+
+    private final CidadeRepository  cidadeRepository;
+
+    public CidadeService(CidadeRepository cidadeRepository) {
+        this.cidadeRepository = cidadeRepository;
+    }
+
+    public ResponseCidade salvar(RequestCidade requestCidade){
+        System.out.println(requestCidade);
+        Cidade novaCidade = new Cidade();
+
+        novaCidade.setNome(requestCidade.nome());
+        novaCidade.setEstado(requestCidade.estado());
+
+        Cidade cidadeSalvar = cidadeRepository.save(novaCidade);
+
+        return toResponseCidade(cidadeSalvar);
+    }
+
+    private ResponseCidade toResponseCidade (Cidade cidade){
+        return  new ResponseCidade(
+                cidade.getId(),
+                cidade.getNome(),
+                cidade.getEstado()
+        );
+    }
 }
