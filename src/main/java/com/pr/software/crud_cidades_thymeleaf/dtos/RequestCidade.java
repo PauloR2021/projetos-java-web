1ef5cc0d98d60eaf65236792cb01abbec1a14eee
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-09-07 21:37:33 -0300

    dtos responsaveis por chamar as requicoes em json com os atributos para criar uma cidade e os retornar em json para a api

--- src/main/java/com/pr/software/crud_cidades_thymeleaf/dtos/RequestCidade.java
+++ src/main/java/com/pr/software/crud_cidades_thymeleaf/dtos/RequestCidade.java
@@ -1,4 +1,16 @@
+/**
+ * Linguagem: Java 25
+ * IDE: IntelliJ IDEA
+ * <p>Record responsável por enviar o JSON de requisição das informações para cadastrar uma nova Cidade</p>
+ *
+ * @author Paulo Ricardo Soares
+ */
+
+
 package com.pr.software.crud_cidades_thymeleaf.dtos;
 
-public record RequestCidade() {
+public record RequestCidade(
+        String nome,
+        String estado
+) {
 }
--- src/main/java/com/pr/software/crud_cidades_thymeleaf/dtos/ResponseCidade.java
+++ src/main/java/com/pr/software/crud_cidades_thymeleaf/dtos/ResponseCidade.java
@@ -1,4 +1,18 @@
+/**
+ * Linguagem: Java 25
+ * IDE: IntelliJ IDEA
+ * <p>Record responsável retornar as informações de uma cidade</p>
+ * <p>Retorna a 'ID' de cadastro, nome e estado</p>
+ *
+ * @author Paulo Ricardo Soares
+ */
+
+
 package com.pr.software.crud_cidades_thymeleaf.dtos;
 
-public record ResponseCidade() {
+public record ResponseCidade(
+        Long id,
+        String nome,
+        String estado
+) {
 }
