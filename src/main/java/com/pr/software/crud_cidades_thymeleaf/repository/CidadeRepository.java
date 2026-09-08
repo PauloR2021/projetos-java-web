cd8e7615408c6a4276ac8c801f30394c951c4705
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-09-07 21:39:41 -0300

    interface responsavel por realizar a conexao com o banco de dados para que seja possivel realizar selects estruturados

--- src/main/java/com/pr/software/crud_cidades_thymeleaf/repository/CidadeRepository.java
+++ src/main/java/com/pr/software/crud_cidades_thymeleaf/repository/CidadeRepository.java
@@ -1,4 +1,16 @@
+/**
+ * Linguagem: Java 25
+ * IDE: IntelliJ IDEA
+ * <p>Classe responsável faz a gestão de alguns tratamento de SQL dentro do banco de dados </p>
+ * <p>Faz os Selects formatados, para não tratar diretamente no Service</p>
+ *
+ * @author Paulo Ricardo Soares
+ */
+
 package com.pr.software.crud_cidades_thymeleaf.repository;
 
-public interface CidadeRepository {
+import com.pr.software.crud_cidades_thymeleaf.model.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CidadeRepository extends JpaRepository<Cidade,Long> {
 }
