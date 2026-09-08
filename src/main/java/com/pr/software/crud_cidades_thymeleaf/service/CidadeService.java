18b7c1993e1b028c681ff2c5725eb2e9b7864f75
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-09-07 21:40:28 -0300

    classe responsavel por gerenciar toda a regra de negocio, aonde vai estar os tratamento de dados

--- src/main/java/com/pr/software/crud_cidades_thymeleaf/service/CidadeService.java
+++ src/main/java/com/pr/software/crud_cidades_thymeleaf/service/CidadeService.java
@@ -1,4 +1,51 @@
+/**
+ * Linguagem: Java 25
+ * IDE: IntelliJ IDEA
+ * <p>Classe responsável por validar a regra de negócio do projeto</p>
+ * <p>Essa classe faz o tratamento de dados para Salvar, Alterar, Excluir e Listar as cidades do Banco</p>
+ *
+ * @author Paulo Ricardo Soares
+ */
+
 package com.pr.software.crud_cidades_thymeleaf.service;
 
+import com.pr.software.crud_cidades_thymeleaf.dtos.RequestCidade;
+import com.pr.software.crud_cidades_thymeleaf.dtos.ResponseCidade;
+import com.pr.software.crud_cidades_thymeleaf.model.Cidade;
+import com.pr.software.crud_cidades_thymeleaf.repository.CidadeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CidadeService {
+
+    private final CidadeRepository cidadeRepository;
+
+    public CidadeService(CidadeRepository cidadeRepository) {
+        this.cidadeRepository = cidadeRepository;
+    }
+
+    //Salvando a nova cidade no banco de dados
+    public ResponseCidade salvaCidade (RequestCidade requestCidade){
+        Cidade cidade = new Cidade();
+        cidade.setNome(requestCidade.nome());
+        cidade.setEstado(requestCidade.estado());
+        Cidade novaCidade = cidadeRepository.save(cidade);
+        return toResponseCidade(novaCidade);
+    }
+
+    //Listando todas as cidades que tem no banco de dados
+    public List<ResponseCidade> listarTodos(){
+        return cidadeRepository.
+                findAll()
+                .stream()
+                .map(this::toResponseCidade)
+                .toList();
+    }
+
+
+    private ResponseCidade toResponseCidade(Cidade cidade) {
+        return new ResponseCidade(cidade.getId(), cidade.getNome(), cidade.getEstado());
+    }
 }
