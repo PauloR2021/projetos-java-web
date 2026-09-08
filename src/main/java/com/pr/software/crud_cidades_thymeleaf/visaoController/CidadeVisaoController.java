90d8506e6faa5f76ed5beedcca8f5f6e6e6b7882
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-09-07 21:41:08 -0300

    classe responsavel por gerencar os endpoints para os utilizadores da aplicacao

--- src/main/java/com/pr/software/crud_cidades_thymeleaf/visaoController/CidadeVisaoController.java
+++ src/main/java/com/pr/software/crud_cidades_thymeleaf/visaoController/CidadeVisaoController.java
@@ -1,4 +1,37 @@
-package com.pr.software.crud_cidades_thymeleaf.ControllerVisao;
+/**
+ * Linguagem: Java 25
+ * IDE: IntelliJ IDEA
+ * <p>Classe responsável por gerir os Endpoints para o utilizador.</p>
+ * <p>Vai armazenar os endpoints que redireciona as páginas HTML</p>
+ * @author Paulo Ricardo Soares
+ */
 
+
+
+
+
+package com.pr.software.crud_cidades_thymeleaf.visaoController;
+import com.pr.software.crud_cidades_thymeleaf.service.CidadeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class CidadeVisaoController {
+
+    private final CidadeService cidadeService;;
+
+    public CidadeVisaoController(CidadeService cidadeService) {
+        this.cidadeService = cidadeService;
+    }
+
+    //Endpoint de Index, para chamar o HTML da página principal
+    @GetMapping("/")
+    public ModelAndView index(){
+        ModelAndView mv = new ModelAndView("index");
+
+        //Através do Thymeleaf já consigo chamar a lista de cidades e mostrar no Index
+        mv.addObject("cidades", cidadeService.listarTodos());
+        return mv;
+    }
 }
