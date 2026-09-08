56803cb83e89d341a6d00a3c0fe94a1b20b9dda7
PauloR2021 <paulotrindadeoficial@gmail.com>
2026-09-07 22:26:57 -0300

    modified: Adicioando o redirecionamento da pagina index para a pagina de alterar cidades

--- src/main/java/com/pr/software/crud_cidades_thymeleaf/visaoController/CidadeVisaoController.java
+++ src/main/java/com/pr/software/crud_cidades_thymeleaf/visaoController/CidadeVisaoController.java
@@ -6,14 +6,12 @@
  * @author Paulo Ricardo Soares
  */
 
-
-
-
-
 package com.pr.software.crud_cidades_thymeleaf.visaoController;
+import com.pr.software.crud_cidades_thymeleaf.dtos.ResponseCidade;
 import com.pr.software.crud_cidades_thymeleaf.service.CidadeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -34,4 +32,16 @@ public class CidadeVisaoController {
         mv.addObject("cidades", cidadeService.listarTodos());
         return mv;
     }
+
+    //Endpoint para ser redirecionado ao HTML de editar
+    @GetMapping("/cidades/editar/{id}")
+    public ModelAndView editar(@PathVariable Long id){
+        ModelAndView mv = new ModelAndView("alterar_cidade"); //Chama a página de Editar
+
+        ResponseCidade cidade = cidadeService.buscarPorId(id); //Passa para o Service qual é o 'ID' para Editar
+
+        mv.addObject("cidade", cidade); //Retornar a Cidade com o 'ID'
+        return mv;
+    }
+
 }
